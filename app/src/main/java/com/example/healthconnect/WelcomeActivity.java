0bbcfbe929d4a974bd7e1a9790251d01b029c2de
William Delmanto <67394462+WRDelmanto@users.noreply.github.com>
2024-11-05 17:13:28 -0800

    Creation of Welcome Activity
    - Removed `MainActivity` to allow more descriptive class naming
    - Created `WelcomeActivity` as the initial screen
    - Added custom colors and strings for consistent patterns
    - Created `gradient_background` to be main background gradient
    - Added `rounded_button` style to be the main rounded button
    
    Missing features:
    - activity_welcome.xml
    Top left canva image
    
    - WelcomeActivity
    Start the next activity (Homepage)

--- app/src/main/java/com/example/healthconnect/WelcomeActivity.java
+++ app/src/main/java/com/example/healthconnect/WelcomeActivity.java
@@ -4,21 +4,28 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        AppCompatButton continueButton = findViewById(R.id.welcome_activity_continue_button);
+        continueButton.setOnClickListener(v -> {
+            // TODO: Start the next activity
+            finish();
         });
     }
 }
