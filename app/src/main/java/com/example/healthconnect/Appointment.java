648715dfa8b6686c41a872383a09d84ce648c68f
William Delmanto <67394462+WRDelmanto@users.noreply.github.com>
2024-11-15 23:16:50 -0800

    Adjust Appointments to Prepare for the CurrentAppointmentActivity Creation

--- app/src/main/java/com/example/healthconnect/Appointment.java
+++ app/src/main/java/com/example/healthconnect/Appointment.java
@@ -7,13 +7,20 @@ public class Appointment implements Serializable {
     private Patient patient;
     private String appointmentType;
     private String appointmentDate;
+    private String appointmentTime;
+    private String notes;
+    private String medicines;
+    private String exams;
 
-    public Appointment(int id, Patient patient, String appointmentType, String appointmentDate, String appointmentTime) {
+    public Appointment(int id, Patient patient, String appointmentType, String appointmentDate, String appointmentTime, String notes, String medicines, String exams) {
         this.id = id;
         this.patient = patient;
         this.appointmentType = appointmentType;
         this.appointmentDate = appointmentDate;
         this.appointmentTime = appointmentTime;
+        this.notes = notes;
+        this.medicines = medicines;
+        this.exams = exams;
     }
 
     public int getId() {
@@ -56,5 +63,27 @@ public class Appointment implements Serializable {
         this.appointmentTime = appointmentTime;
     }
 
-    private String appointmentTime;
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getMedicines() {
+        return medicines;
+    }
+
+    public void setMedicines(String medicines) {
+        this.medicines = medicines;
+    }
+
+    public String getExams() {
+        return exams;
+    }
+
+    public void setExams(String exams) {
+        this.exams = exams;
+    }
 }
--- app/src/main/java/com/example/healthconnect/MockAppointments.java
+++ app/src/main/java/com/example/healthconnect/MockAppointments.java
@@ -9,18 +9,18 @@ public class MockAppointments {
     static {
         List<Patient> mockPatients = MockPatients.getMockPatients();
 
-        mockAppointments.add(new Appointment(1, mockPatients.get(0), "New patient", "15/11/2024", "09:30"));
-        mockAppointments.add(new Appointment(2, mockPatients.get(1), "New patient", "16/11/2024", "10:00"));
-        mockAppointments.add(new Appointment(3, mockPatients.get(2), "New patient", "17/11/2024", "10:20"));
-        mockAppointments.add(new Appointment(4, mockPatients.get(3), "New patient", "18/11/2024", "10:40"));
-        mockAppointments.add(new Appointment(5, mockPatients.get(0), "Regular appointment", "19/11/2024", "09:30"));
-        mockAppointments.add(new Appointment(6, mockPatients.get(1), "Regular appointment", "20/11/2024", "10:00"));
-        mockAppointments.add(new Appointment(7, mockPatients.get(2), "Regular appointment", "21/11/2024", "10:20"));
-        mockAppointments.add(new Appointment(8, mockPatients.get(3), "Regular appointment", "22/11/2024", "10:40"));
-        mockAppointments.add(new Appointment(9, mockPatients.get(0), "Regular appointment", "23/11/2024", "09:30"));
-        mockAppointments.add(new Appointment(10, mockPatients.get(1), "Regular appointment", "24/11/2024", "10:00"));
-        mockAppointments.add(new Appointment(11, mockPatients.get(2), "Regular appointment", "25/11/2024", "10:20"));
-        mockAppointments.add(new Appointment(12, mockPatients.get(3), "Regular appointment", "26/11/2024", "10:40"));
+        mockAppointments.add(new Appointment(1, mockPatients.get(0), "New patient", "15/11/2024", "09:30", "", "", ""));
+        mockAppointments.add(new Appointment(2, mockPatients.get(1), "New patient", "16/11/2024", "10:00", "", "", ""));
+        mockAppointments.add(new Appointment(3, mockPatients.get(2), "New patient", "17/11/2024", "10:20", "", "", ""));
+        mockAppointments.add(new Appointment(4, mockPatients.get(3), "New patient", "18/11/2024", "10:40", "", "", ""));
+        mockAppointments.add(new Appointment(5, mockPatients.get(0), "Regular appointment", "19/11/2024", "09:30", "", "", ""));
+        mockAppointments.add(new Appointment(6, mockPatients.get(1), "Regular appointment", "20/11/2024", "10:00", "", "", ""));
+        mockAppointments.add(new Appointment(7, mockPatients.get(2), "Regular appointment", "21/11/2024", "10:20", "", "", ""));
+        mockAppointments.add(new Appointment(8, mockPatients.get(3), "Regular appointment", "22/11/2024", "10:40", "", "", ""));
+        mockAppointments.add(new Appointment(9, mockPatients.get(0), "Regular appointment", "23/11/2024", "09:30", "", "", ""));
+        mockAppointments.add(new Appointment(10, mockPatients.get(1), "Regular appointment", "24/11/2024", "10:00", "", "", ""));
+        mockAppointments.add(new Appointment(11, mockPatients.get(2), "Regular appointment", "25/11/2024", "10:20", "", "", ""));
+        mockAppointments.add(new Appointment(12, mockPatients.get(3), "Regular appointment", "26/11/2024", "10:40", "", "", ""));
     }
 
     public static List<Appointment> getMockAppointments() {
